4208e64ee86ba27b86117463fcf7c1d2d233708b
Ashik A S <ashikanil500@gmail.com>
2026-08-31 23:53:22 +0530

    Update app.py

--- vulnerable_student_app/app.py
+++ vulnerable_student_app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import sqlite3
-#tesfdfdhfhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+#tesfdfdhfhhhhhhhhhhhhhhhhhhhhhhhhhhhhfgfdffddhhhhh
 # Initialize In-Memory Database for local testing
 def init_db():
     conn = sqlite3.connect(":memory:", check_same_thread=False)
