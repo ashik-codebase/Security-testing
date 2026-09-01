f91ff82bc0b939d75fd7cb24a9dbe6c47dc54fa1
Ashik A S <ashikanil500@gmail.com>
2026-09-01 18:41:15 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import hashlib
 import subprocess
-#test 4656
+#test 4656gdfx
 def get_user_profile(username: str):
     """
     VULNERABILITY 1: SQL Injection (OWASP A03:2021 / CWE-89)
